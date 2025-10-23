d7ff88d75ace211ce4a013fb4968959a3a6bf627
cosimoNigro <cosimonigro2@gmail.com>
2025-10-23 17:33:43 +0200

    created a Source class

--- build_basic_catalogue.py
+++ build_basic_catalogue.py
@@ -4,13 +4,12 @@
 import logging
 from astroquery.vizier import Vizier
 # - local imports
-from data_structures import Source
+from data_structures2 import Source
 
 # set up logging
-log = logging.getLogger(__name__)
-logging.basicConfig(
+log = logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s|%(level)s|%(name)s|%(message)s",
+    format="%(asctime)s|%(levelname)s|%(name)s|%(message)s",
     datefmt="%m/%d/%Y %I:%M:%S %p",
 )
 
@@ -20,5 +19,11 @@ ho_1997 = Vizier(columns=["**"], row_limit=-1).get_catalogs("J/ApJS/112/315")
 nagar_2005 = Vizier(columns=["**"], row_limit=-1).get_catalogs("J/A+A/435/521")
 # - FR0 Catalog
 fr0cat = Vizier(columns=["**"], row_limit=-1).get_catalogs("J/A+A/609/A1")
+
+source1 = Source("NGC 1275")
+print(source1.ned_flux_table)
+
+source2 = Source("NGC 4261")
+print(source2.ned_flux_table)
 
 import IPython; IPython.embed()
